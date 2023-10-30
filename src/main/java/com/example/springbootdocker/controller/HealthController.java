d31334a3d7c82cf96fd4ce15c46169201a2a9c6a
724782016@qq.com <weifei062201030>
2023-10-30 18:13:23 +0800

    springboot docker first commit

---
+++ src/main/java/com/example/springbootdocker/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.example.springbootdocker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @ResponseBody
+    @GetMapping("/health")
+    public String health(){
+        return "ok";
+    }
+}
